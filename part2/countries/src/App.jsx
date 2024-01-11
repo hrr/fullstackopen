cdaa1fcefc73984dc7e5e9e4cca61bca415a391c
hrr <jia@qiu.io>
2024-01-11 16:00:24 +0800

    2.18

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+import countryService from './services/countries'
+import Country from './components/Country'
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [searchName, setSearchName] = useState('')
+  const [matches, setMatches] = useState(0)
+
+  const handleSearchNameChange = (event) => {
+      const newSearchName = event.target.value
+      setSearchName(newSearchName)
+      console.log(countries.filter(c => c.name.common.toLowerCase().includes(newSearchName.toLowerCase())).length)
+      setMatches(countries.filter(c => c.name.common.toLowerCase().includes(newSearchName.toLowerCase())))
+  }
+
+  useEffect(() => {
+    countryService
+      .getAll()
+      .then(initialCountries => {
+        setCountries(initialCountries)
+      })
+  }, [])
+
+  useEffect(() => {
+    setMatches(countries.filter(c => c.name.common.toLowerCase().includes(searchName.toLowerCase())))    
+  }, [countries])
+  
+  return (
+    <div>
+      find countries <input value={searchName} onChange={handleSearchNameChange} />
+      {searchName.length > 0 && matches.length > 10 && <div>Too many matches, specify another filter</div>}
+      {searchName.length > 0 && matches.length <= 10 && matches.length !== 1 && matches.map(c => <div key={c.name.common}>{c.name.common}</div>)}
+      {searchName.length > 0 && matches.length === 1 && matches.map(c => <Country country={matches[0]}/>)}
+    </div>
+  )
+}
+
+export default App
---
+++ part2/countries/src/components/Country.jsx
@@ -0,0 +1,12 @@
+const Country = ({ country }) => {
+    return <div>
+        <h1>{country.name.common}</h1>
+        <div>capital {country.capital[0]}</div>
+        <div>area {country.area}</div>
+        <h2>languages:</h2>
+        {Object.keys(country.languages).map(key => <li key={(key)}>{country.languages[key]}</li>)}
+        <div><img src={country.flags.png} /></div>
+    </div>
+}
+
+export default Country

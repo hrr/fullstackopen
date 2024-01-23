8c6e0a48d0b7c46cbbf20abbf73b4f2fcace522b
hrr <jia@qiu.io>
2024-01-23 17:58:01 +0800

    5.7

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,7 +1,29 @@
-const Blog = ({ blog }) => (
-  <div>
+import { useRef } from 'react'
+import Togglable from './Togglable'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const blogToggleRef = useRef()
+  const blogToggle = () => {
+    blogToggleRef.current.toggleVisibility()
+  }
+
+  return <div style={blogStyle}>
     {blog.title}
-  </div>  
-)
+    <Togglable buttonLabel="view" ref={blogToggleRef}>
+      <div>{blog.content}</div>
+      <div>{blog.url}</div>
+      <div>likes {blog.likes}<button>like</button></div>
+      <div>{blog.user.name}</div>
+    </Togglable>
+  </div>
+}
 
 export default Blog

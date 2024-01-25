080fe68b86df4f8c230f78c35e4958f103e277ba
hrr <jia@qiu.io>
2024-01-25 10:12:42 +0800

    6.1

---
+++ part6/src/reducer.js
@@ -0,0 +1,25 @@
+const initialState = {
+  good: 0,
+  ok: 0,
+  bad: 0
+}
+
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'GOOD':
+      state = { ...state, good: state.good + 1 }
+      break
+    case 'OK':
+      state = { ...state, ok: state.ok + 1 }
+      break
+    case 'BAD':
+      state = { ...state, bad: state.bad + 1 }
+      break
+    case 'ZERO':
+      break
+  }
+  return state
+  
+}
+
+export default counterReducer
